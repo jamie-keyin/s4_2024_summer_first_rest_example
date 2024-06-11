2f3d07aa5900882fdfea8eae82d6387ef9d0a091
Jamie Cornick <jamie.cornick@keyin.com>
2024-06-11 10:20:53 -0230

    Added a map to store greetings in memory and some new endpoints.

--- src/main/java/com/keyin/hello/GreetingController.java
+++ src/main/java/com/keyin/hello/GreetingController.java
@@ -1,10 +1,10 @@
 package com.keyin.hello;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -15,5 +15,25 @@ public class GreetingController {
     @GetMapping("hello")
     public Greeting greeting(@RequestParam(value = "name", required = false) String name) {
         return greetingService.generateGreeting(name);
+    }
+
+    @GetMapping("search_greeting")
+    public Greeting searchGreeting(@RequestParam(value = "index", required = false) Integer index) {
+        return greetingService.getGreeting(index);
+    }
+
+    @GetMapping("greetings")
+    public List<Greeting> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+
+    @GetMapping("greeting/{index}")
+    public Greeting getGreeting(@PathVariable Integer index) {
+        return greetingService.getGreeting(index);
+    }
+
+    @PostMapping("greeting")
+    public Greeting createGreeting(@RequestBody Greeting newGreeting) {
+        return greetingService.createGreeting(newGreeting);
     }
 }
--- src/main/java/com/keyin/hello/GreetingService.java
+++ src/main/java/com/keyin/hello/GreetingService.java
@@ -3,8 +3,14 @@ package com.keyin.hello;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class GreetingService {
+    private Map<Integer, Greeting> greetingMap = new HashMap<Integer, Greeting>();
+
     public Greeting generateGreeting(String name) {
         Greeting greeting = new Greeting();
         greeting.setGreeting("Hello");
@@ -18,4 +24,17 @@ public class GreetingService {
         return greeting;
     }
 
+    public Greeting getGreeting(Integer index) {
+        return greetingMap.get(index);
+    }
+
+    public Greeting createGreeting(Greeting newGreeting) {
+        greetingMap.put(greetingMap.size() + 1, newGreeting);
+
+        return newGreeting;
+    }
+
+    public List<Greeting> getAllGreetings() {
+        return List.copyOf(greetingMap.values());
+    }
 }
